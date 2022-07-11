1f82006f5c9540130ee2fcc104f774c373c825e7
Kavandeep <kavan1@live.co.uk>
2022-07-11 15:59:48 +0100

    tests for morse tranlsate morse code

--- script.spec.js
+++ script.spec.js
@@ -1,1 +1,19 @@
 import {expect, it} from "@jest/globals";
+
+import { translateToMorse } from "./script.js";
+
+// positive tests
+it("should translate hello to .... . .-.. .-.. ---", () => {
+    const result = translateToMorse("hello");
+    expect(result).toBe(".... . .-.. .-.. ---");
+});
+
+it("should translate bye to -... -.-- .", () => {
+    const result = translateToMorse("bye");
+    expect(result).toBe("-... -.-- .");
+});
+
+it("should translate hello how are you  -- -.-- / -. .- -- . / .. ... / -.- .- ...-", () => {
+    const result = translateToMorse("my name is kav");
+    expect(result).toBe("-- -.-- / -. .- -- . / .. ... / -.- .- ...-");
+});
